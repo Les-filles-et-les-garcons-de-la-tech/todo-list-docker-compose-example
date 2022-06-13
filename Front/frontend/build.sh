f7d625a3323122fa55ee0338a314cdd0d8728761
herve <rvfontbonne@gmail.com>
2022-06-13 20:11:03 +0200

    add image

--- Front/frontend/build.sh
+++ Front/frontend/build.sh
@@ -1,3 +1,23 @@
+#!/bin/bash
+
+#####################
+# Version Classique #
+#####################
 npm install
 npm run build && \
 docker build -t frontend:1.0 ../
+
+
+#####################
+# Version dynamique #
+#####################
+# npm install
+
+# for i in {10..15}; do
+#     TAG=stu$i
+#     npm run build -- --base-href /"$TAG"/ && \
+#     docker build -t fgtech/frontend-todolist:"$TAG" ../
+
+#     docker push fgtech/frontend-todolist:"$TAG"
+ 
+# done
